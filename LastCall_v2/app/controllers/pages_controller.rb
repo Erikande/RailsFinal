a2dc7550f538e8a0ed6243cea65f457394d66efd
Tedi Konda <tedikonda@gmail.com>
2014-07-10 00:03:42 -0400

    Cleaning up show view and show action.

--- LastCall_v2/app/controllers/pages_controller.rb
+++ LastCall_v2/app/controllers/pages_controller.rb
@@ -9,27 +9,32 @@ class PagesController < ApplicationController
   end
   
   def inside
-  	@places = current_user.user_places
+    @places = current_user.user_places
 
-  	# UserPlace.find(:all, :conditions => 'id > 0')
+    # UserPlace.find(:all, :conditions => 'id > 0')
   end 
 
   def show
-   	@place = UserPlace.find(params[:id])
-    @lat = @place.latitude
-    @lng = @place.longitude
-    @uri = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{@lat},#{@lng}&radius=500&types=train_station&key=AIzaSyApJV1Xj5hGnm9DMxIE-N-UfEioqeT_RRc"
+    @place = UserPlace.find(params[:id])
+    @uri = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{@place.latitude},#{@place.longitude}&radius=500&types=train_station&key=AIzaSyApJV1Xj5hGnm9DMxIE-N-UfEioqeT_RRc"
     @response = JSON.parse(RestClient.get @uri)
     @areas = @response['results']
-
+    @parsed_areas = []
+    @areas.each do |area|
+      current_area = {
+        name: area["name"],
+        vicinity: area["vicinity"]
+      }
+      @parsed_areas << current_area
+    end
   end
 
 
   def destroy
-  	@place = UserPlace.find(params[:id])
-  	@place.destroy
+    @place = UserPlace.find(params[:id])
+    @place.destroy
 
-  	redirect_to inside_path
+    redirect_to inside_path
   end
     
 end
