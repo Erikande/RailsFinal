8fccaace9c3a75c16ef1f54bffd9a6852a302b57
Erik <erikande@gmail.com>
2014-07-09 22:14:13 -0400

    updating views

--- LastCall_v2/app/controllers/pages_controller.rb
+++ LastCall_v2/app/controllers/pages_controller.rb
@@ -1,5 +1,9 @@
 class PagesController < ApplicationController
   before_action :authenticate_user!, only: [:inside, :inside_new]
+  # require 'dish'
+  require 'json'
+  require 'rest_client'
+  require "uri"
 
   def home
   end
@@ -11,8 +15,15 @@ class PagesController < ApplicationController
   end 
 
   def show
- 	@place = UserPlace.find(params[:id])
+   	@place = UserPlace.find(params[:id])
+    @lat = @place.latitude
+    @lng = @place.longitude
+    @uri = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{@lat},#{@lng}&radius=500&types=train_station&key=AIzaSyApJV1Xj5hGnm9DMxIE-N-UfEioqeT_RRc"
+    @response = JSON.parse(RestClient.get @uri)
+    @areas = @response['results']
+
   end
+
 
   def destroy
   	@place = UserPlace.find(params[:id])
