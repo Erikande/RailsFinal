10ccaeb91cb58047c58b492a3f669092802b831e
Erik <erikande@gmail.com>
2014-07-10 05:04:40 -0400

    commit

--- LastCall_v2/app/controllers/pages_controller.rb
+++ LastCall_v2/app/controllers/pages_controller.rb
@@ -19,10 +19,10 @@ class PagesController < ApplicationController
     @uri = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{@place.latitude},#{@place.longitude}&radius=500&types=train_station&key=AIzaSyApJV1Xj5hGnm9DMxIE-N-UfEioqeT_RRc"
     @response = JSON.parse(RestClient.get @uri)
     @areas = @response['results']
-<<<<<<< HEAD
 
 
-=======
+
+
     @parsed_areas = []
     @areas.each do |area|
       current_area = {
@@ -31,7 +31,6 @@ class PagesController < ApplicationController
       }
       @parsed_areas << current_area
     end
->>>>>>> a2dc7550f538e8a0ed6243cea65f457394d66efd
   end
 
 
